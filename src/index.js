5f6b1747944eecc4fffe4f0dfe4f0ee0501dbbf7
SergeyZubkov <Coh1bAAAA@mail.ru>
2017-07-13 14:55:28 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+import './index.css';
+import './sliding_line.css'
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+import $ from 'jquery';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+console.log($)
